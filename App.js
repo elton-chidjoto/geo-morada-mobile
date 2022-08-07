64752f52fc42ff74b82c6e2f98b2c4cf96dd1e95
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-08-07 10:03:25 +0200

    Adding files to green zone

--- App.js
+++ App.js
@@ -1,112 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+//import React from 'react';
+//import {View,Text} from 'react-native'
+//import { NavigationContainer } from '@react-navigation/native'
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import {Splash,Onboarding,Login} from './src/screens'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React,{useState} from 'react'
+import { StyleSheet, Text, ScrollView,View,StatusBar,Image,TextInput, TouchableOpacity } from 'react-native'
+import {Colors} from './src/constants'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import Buttons from './src/components/Buttons'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//const Stack = createNativeStackNavigator();
+const App = ()=>{
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const [formData,setformData] = useState({
+        email:'',
+        password:''
+    })
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+          <ScrollView style={{flex:1,backgroundColor:'#fff',flexDirection:'column'}}>
+              <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+              {/* login form section */}
+              <View style={{flex:2,flexDirection:'column',backgroundColor:'#fff',paddingTop:10,paddingHorizontal:'3%'}} >
+                  <View style={{flexDirection:'row',justifyContent:'flex-start',alignItems:'center'}} >
+                      <Image source={require('./src/assets/images/waving_hand.png')} style={{width:30,height:30}}  />
+                                            <Text style={{fontFamily:'OpenSans-SemiBold',fontSize:30,color:Colors.black}} > GeoMorada</Text>
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+                  </View>
+                  <Text style={{fontFamily:"OpenSans-Regular",fontSize:14,paddingTop:10,color:"#777"}} >Estou feliz em te ver novamente. Voce pode continuar de onde voçe parou fazendo o login aqui.</Text>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+                  <View style={{flexDirection:'column',paddingTop:20}} >
+                      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',backgroundColor:'#ededed',width:'95%',borderRadius:10,height:60,paddingLeft:20}} >
+                          <Icon name="envelope-o" size={22} color="#818181" />
+                          <TextInput onChangeText={(text)=>{setformData((prevState)=>({...prevState,email:text}))}} style={styles.input} placeholder="Enter Email" placeholderTextColor="#818181" />
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+                      </View>
+
+                      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',backgroundColor:'#ededed',width:'95%',borderRadius:10,height:60,paddingLeft:20,marginTop:20}} >
+                          <Icon name="lock" size={22} color="#818181" />
+                          <TextInput onChangeText={(text)=>{setformData((prevState)=>({...prevState,password:text}))}} style={styles.input} placeholder="Enter Password" secureTextEntry={true} placeholderTextColor="#818181" />
+                      </View>
+
+                      <View style={{width:'95%',marginBottom:10}} >
+                          <Text style={{fontSize:17,fontFamily:'OpenSans-SemiBold',
+                      color:'#818181',alignSelf:'flex-end',paddingTop:10}} >Forgot Password?</Text>
+                      </View>
+
+                      <Buttons  btn_text={"Sign In"} on_press={()=>console.log(formData)} />
+                  </View>
+              </View>
+
+              {/* social login section */}
+              <View style={{flex:2,backgroundColor:'#fff',flexDirection:'column',paddingHorizontal:'3%'}} >
+                  <Text style={{fontFamily:"OpenSans-Bold",textAlign:'center',marginVertical:35,color:'#818181',fontSize:20}} >Or</Text>
+
+                  <View style={{flexDirection:'column',alignItems:'center',width:'95%'}} >
+                      <TouchableOpacity onPress={()=>console.log("google login")} style={styles.social_btn} >
+                          <Image style={styles.social_img} source={require('./src/assets/images/google_icon.png')} />
+                          <Text style={{width:'80%',textAlign:'center',fontSize:16,fontFamily:'OpenSans-Medium'}} >Sign in with Google </Text>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity onPress={()=>console.log("facebook login")} style={styles.social_btn} >
+                          <Image style={styles.social_img} source={require('./src/assets/images/facebook_icon.png')} />
+                          <Text style={{width:'80%',textAlign:'center',fontSize:16,fontFamily:'OpenSans-Medium'}} >Sign in with Facebook </Text>
+                      </TouchableOpacity>
+                  </View>
+
+                  <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'flex-end',backgroundColor:'#fff',marginBottom:40}} >
+                      <Text style={{fontFamily:'OpenSans-Medium',fontSize:17,color:'#818181'}} >Don't have a account? </Text>
+                      <Text style={{fontSize:18,fontFamily:'OpenSans-SemiBold',color:'#333'}} >Sign Up</Text>
+                  </View>
+
+
+
+              </View>
+
+          </ScrollView>
+      );
+}
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    input:{
+        position:'relative',
+        height:'100%',
+        width:'90%',
+        fontFamily:'OpenSans-Medium',
+        paddingLeft:20,
+    },
+    social_btn:{
+        height:55,
+        width:'100%',
+        borderWidth:1,
+        borderRadius:10,
+        borderColor:'#ddd',
+        flexDirection:'row',
+        alignItems:'center',
+        marginBottom:20
+    },
+    social_img:{
+        width:25,
+        height:25,
+        marginLeft:15
+    }
+})
---
+++ src/constants/Colors.js
@@ -0,0 +1,6 @@
+export default Colors ={
+    primary:'#46d68e',
+    input_bg:'3f3f5f7',
+    black:'#333',
+    white:'#fff'
+}
